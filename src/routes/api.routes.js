2c5a2608c650d6d45e0567115875bab935524586
95sonia <95sonia@gmail.com>
2026-01-27 17:21:21 +0100

    cambio en controladores apis unificadas

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -6,13 +6,7 @@ const { checksValidaciones } = require("../middlewares/checkValidations")
 const { verificarRol } = require('../middlewares/verificarRol');
 const upload = require('../middlewares/upload');
 const {
-    getTemperatura,
-    getHumedadRelativa,
-    getHumedadSuelo,
-    getPrecipitacion,
-    getVientoVelocidad,
-    getVientoDireccion,
-    getEvapotranspiracion,
+    getAllMediciones,
     getHistoricoPorFechas,
     getAlertaPlagas,
     getAnalisisClimatico,
@@ -23,13 +17,7 @@ const {
     chatAsistente
 } = require('../controllers/api.controller')
 
-router.post('/temperatura', [verificarRol(['Asesor', 'Productor', 'Manager', 'Administrador'])], getTemperatura);
-router.post('/humedad-relativa', [verificarRol(['Asesor', 'Productor', 'Manager', 'Administrador'])], getHumedadRelativa);
-router.post('/humedad-suelo', [verificarRol(['Asesor', 'Productor', 'Manager', 'Administrador'])], getHumedadSuelo);
-router.post('/precipitacion', [verificarRol(['Asesor', 'Productor', 'Manager', 'Administrador'])], getPrecipitacion);
-router.post('/viento-velocidad', [verificarRol(['Asesor', 'Productor', 'Manager', 'Administrador'])], getVientoVelocidad);
-router.post('/viento-direccion', [verificarRol(['Asesor', 'Productor', 'Manager', 'Administrador'])], getVientoDireccion);
-router.post('/evapotranspiracion', [verificarRol(['Asesor', 'Productor', 'Manager', 'Administrador'])], getEvapotranspiracion);
+router.post('/mediciones/:variable', [verificarRol(['Asesor', 'Productor', 'Manager', 'Administrador'])], getAllMediciones);
 router.post('/historico', [verificarRol(['Asesor', 'Productor', 'Manager', 'Administrador'])], getHistoricoPorFechas);
 router.get('/alerta-plagas', [verificarRol(['Asesor', 'Productor', 'Manager', 'Administrador'])], getAlertaPlagas);
 router.get('/analisis-climatico', [verificarRol(['Asesor', 'Productor', 'Manager', 'Administrador'])], getAnalisisClimatico);
